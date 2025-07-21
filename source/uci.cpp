d8e9b3ba1594afe862cdf8c29dd15e85db6850d1
rektdie <rektdie@gmail.com>
2025-07-21 08:23:46 +0200

    added capture history | bench 2725719

--- source/uci.cpp
+++ source/uci.cpp
@@ -143,6 +143,7 @@ void UCILoop(Board &board) {
             ctx->killerMoves = {};
             ctx->history.Clear();
             ctx->conthist.Clear();
+            ctx->capthist.Clear();
             ctx->positionHistory = {};
             ctx->ss = {};
 
